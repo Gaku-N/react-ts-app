ec4597057943050f334e476c7461c494f2bd27c6
Gaku-N <naito@hosiden.com>
2024-09-11 16:58:45 +0900

    ルーターを別ファイルに切り出し

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,1 +1,18 @@
-export const Router1 = () => {};
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from '../components/pages/Home';
+import { Login } from '../components/pages/Login';
+import { Setting } from '../components/pages/Setting';
+import { UserManagement } from '../components/pages/UserManagement';
+export const Router = () => {
+  return (
+    <>
+    <Routes >
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/Login' element={<Login />}></Route>
+        <Route path='/Setting' element={<Setting />}></Route>
+        <Route path='/UserManagement' element={<UserManagement />}></Route>
+      </Routes>
+    </>
+  )
+};
